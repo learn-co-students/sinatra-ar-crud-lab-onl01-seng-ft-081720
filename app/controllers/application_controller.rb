6e445dc84e4ece4b273e230da136e3352f0cf8f6
tjaRoxasXIII <tjaroguetech@gmail.com>
2020-09-30 16:03:55 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,47 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.create(params)
+    # binding.pry
+    redirect "/articles/#{Article.last.id}" #Can't use single quotes here due to the variable
+  end
+
+  get '/articles' do
+    @articles = Article.all
+
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.all.find_by_id(params[:id])
+
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.all.find_by_id(params[:id])
+    # binding.pry
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.all.find_by_id(params[:id])
+    params.delete("_method")
+    if @article.update(params)
+      redirect "/articles/#{@article.id}"
+    else
+      redirect "articles/new"
+    end
+  end
+
+  delete '/articles/:id' do
+    
+  end
+
 end
